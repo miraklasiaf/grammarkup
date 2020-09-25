e9d8f77d48b282387e0c484f306402ca72f25e26
Faisal Karim <faisalkarim96@gmail.com>
2020-09-25 10:21:31 +0700

    fix: forwarding ref in custom tooltip component

--- components/info-tooltip.js
+++ components/info-tooltip.js
@@ -1,10 +1,21 @@
+import { forwardRef } from 'react'
 import { Tooltip } from '@chakra-ui/core'
 import { InfoOutlineIcon } from '@chakra-ui/icons'
 
-export default function InfoTooltip({ label }) {
+const InfoTooltip = forwardRef((props, ref) => {
   return (
-    <Tooltip hasArrow label={label} placement="right" bg="gray.600" color="white">
+    <Tooltip
+      ref={ref}
+      hasArrow
+      label={props.label}
+      placement="right"
+      bg="gray.600"
+      color="white"
+      {...props}
+    >
       <InfoOutlineIcon />
     </Tooltip>
   )
-}
+})
+
+export default InfoTooltip
