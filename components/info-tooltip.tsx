ae8e0f6513e782aade37ec12c0b121db99026672
Faisal Karim <faisalkarim96@gmail.com>
2020-10-16 14:22:49 +0700

    feat: add new lesson - how to form adverbs

--- components/info-tooltip.tsx
+++ components/info-tooltip.tsx
@@ -1,23 +1,21 @@
-import { forwardRef, Ref } from "react"
-import { Tooltip, TooltipProps } from "@chakra-ui/core"
-import { InfoOutlineIcon } from "@chakra-ui/icons"
+import { forwardRef, Ref } from 'react'
+import { Tooltip, TooltipProps } from '@chakra-ui/core'
+import { InfoOutlineIcon } from '@chakra-ui/icons'
 
-const InfoTooltip = forwardRef<TooltipProps, "div">(
-  (props: any, ref: Ref<any>) => {
-    return (
-      <Tooltip
-        ref={ref}
-        hasArrow
-        label={props.children}
-        placement="right"
-        bg="gray.600"
-        color="white"
-        {...props}
-      >
-        <InfoOutlineIcon />
-      </Tooltip>
-    )
-  },
-)
+const InfoTooltip = forwardRef<TooltipProps, 'div'>((props: any, ref: Ref<any>) => {
+  return (
+    <Tooltip
+      ref={ref}
+      hasArrow
+      label={props.children}
+      placement="right"
+      bg="gray.600"
+      color="white"
+      {...props}
+    >
+      <InfoOutlineIcon />
+    </Tooltip>
+  )
+})
 
 export default InfoTooltip
