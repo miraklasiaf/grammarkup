562e76ce8258c68245a5233333aef06e75893a30
Faisal Karim <faisalkarim96@gmail.com>
2020-10-17 22:39:01 +0700

    chore: change layout

--- components/container.tsx
+++ components/container.tsx
@@ -1,7 +1,16 @@
 import { Box } from '@chakra-ui/core'
 
 export const Container = (props) => (
-  <Box w="full" pb="12" pt="3" mx="auto" maxW="1200px" px={6} {...props} />
+  <Box
+    w="full"
+    pb="12"
+    pt="3"
+    mx="auto"
+    maxW="8xl"
+    pl={6}
+    pr={{ base: 6, md: 0 }}
+    {...props}
+  />
 )
 
 export default Container
