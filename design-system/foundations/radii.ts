c36ede3a4e59c7df9d4413dfbf1007ee373b789e
Faisal Karim <faisalkarim96@gmail.com>
2020-10-23 15:02:38 +0700

    refactor: change design system

--- design-system/foundations/radii.ts
+++ design-system/foundations/radii.ts
@@ -1,11 +1,12 @@
-export const radii = {
+const radii = {
   none: '0',
-  sm: '0.125rem', // 2px
-  default: '0.25rem', // 4px
-  md: '0.375rem', // 6px
-  lg: '0.5rem', // 8px
-  xl: '0.75rem', // 12px
-  circle: '9999px'
+  sm: '0.125rem',
+  default: '0.25rem',
+  md: '0.375rem',
+  lg: '0.5rem',
+  full: '9999px'
 }
+
+export type Radii = typeof radii
 
 export default radii
--- design-system/styles/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as layerStyles } from './layer-styles'
-export { default as textStyles } from './text-styles'
