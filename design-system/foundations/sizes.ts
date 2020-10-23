de987a7b2e0c35ecef20f4281018ec6ef46409a9
Faisal Karim <faisalkarim96@gmail.com>
2020-10-23 15:17:20 +0700

    chore: add layouting

--- design-system/foundations/sizes.ts
+++ design-system/foundations/sizes.ts
@@ -70,6 +70,18 @@ const percentageSizes = {
   '11/12': '91.666667%'
 }
 
+const layouting = {
+  widePlus: '128rem', // 2048px
+  wide: '96rem', // 1536
+  layoutPlus: '75rem', // 1200
+  layout: '64rem', // 1024
+  copyUltra: '61.25rem', // 980
+  copyPlus: '48rem', // 768
+  copy: '42.5rem', // 680
+  narrowPlus: '37.5rem', // 600
+  narrow: '32rem' // 512px
+}
+
 const container = {
   sm: '640px',
   md: '768px',
@@ -81,6 +93,7 @@ const sizes = {
   ...space,
   ...largeSizes,
   ...percentageSizes,
+  layouting,
   container
 }
 
--- design-system/index.ts
+++ design-system/index.ts
@@ -6,6 +6,32 @@ const theme = {
   ...chakraTheme,
   ...foundations,
   styles,
+  layout: {
+    container: {
+      maxWidth: ['layout', null, 'layoutPlus'],
+      width: '100%',
+      mx: 'auto',
+      px: 4
+    },
+    wide: {
+      width: '100%',
+      mx: 'auto',
+      px: 4,
+      maxWidth: ['layout', null, 'wide']
+    },
+    copy: {
+      width: '100%',
+      mx: 'auto',
+      px: 4,
+      maxWidth: ['copy', null, 'copyPlus']
+    },
+    narrow: {
+      width: '100%',
+      mx: 'auto',
+      px: 4,
+      maxWidth: ['narrow', null, 'narrowPlus']
+    }
+  },
   textStyles: {
     heading: {
       textAlign: 'center',
