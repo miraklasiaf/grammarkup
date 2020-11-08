d59b469dbfc9aa1945c1a5ab087bfa6135d181e3
Faisal Karim <faisalkarim96@gmail.com>
2020-11-08 20:23:19 +0700

    chore: add gtag pageview event in nprogress

--- components/ui/Nprogress/Nprogress.tsx
+++ components/ui/Nprogress/Nprogress.tsx
@@ -8,7 +8,7 @@ const start = debounce(nprogress.start, 500)
 
 Router.events.on('routeChangeStart', start)
 
-Router.events.on('routeChangeComplete', () => {
+Router.events.on('routeChangeComplete', (url) => {
   start.cancel()
   nprogress.done()
   window.scrollTo(0, 0)
