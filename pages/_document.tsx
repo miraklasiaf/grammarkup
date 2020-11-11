870b182e89a338030210d9f749959f97fb62f9af
Faisal Karim <faisalkarim96@gmail.com>
2020-11-11 12:43:46 +0700

    chore: change favicon color scheme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,34 +21,38 @@ class Document extends NextDocument {
         <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
         <Head>
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          <meta content="#ffffff" name="theme-color" />
-          <meta content="#ffffff" name="msapplication-TileColor" />
-          <meta content="/static/browserconfig.xml" name="msapplication-config" />
+          <meta content="#0694a2" name="theme-color" />
+          <meta content="#0694a2" name="msapplication-TileColor" />
+          <meta content="/static/favicon/browserconfig.xml" name="msapplication-config" />
           <meta
             name="google-site-verification"
             content="KfTyzAKabwlabC29C3S23xuqg6nq_fG94IRlnpFSNtI"
           />
-          <link href="/static/favicon.ico" rel="shortcut icon" />
-          <link href="/static/site.webmanifest" rel="manifest" />
+          <link href="/static/favicon/favicon.ico" rel="shortcut icon" />
+          <link href="/static/favicon/site.webmanifest" rel="manifest" />
           <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
           <link
-            href="/static/apple-touch-icon.png"
+            href="/static/favicon/apple-touch-icon.png"
             rel="apple-touch-icon"
             sizes="180x180"
           />
           <link
-            href="/static/favicon-32x32.png"
+            href="/static/favicon/favicon-32x32.png"
             rel="icon"
             sizes="32x32"
             type="image/png"
           />
           <link
-            href="/static/favicon-16x16.png"
+            href="/static/favicon/favicon-16x16.png"
             rel="icon"
             sizes="16x16"
             type="image/png"
           />
-          <link color="#4a9885" href="/static/safari-pinned-tab.svg" rel="mask-icon" />
+          <link
+            color="#4a9885"
+            href="/static/favicon/safari-pinned-tab.svg"
+            rel="mask-icon"
+          />
           <script
             data-ad-client="ca-pub-7966864664516090"
             async
