1937b511d1a74d0eb7717b39385b88ad61a5940e
Faisal Karim <faisalkarim96@gmail.com>
2020-11-12 14:07:46 +0700

    chore: add google ad unit

---
+++ components/ui/google-ad/google-ad.tsx
@@ -0,0 +1,21 @@
+//@ts-nocheck
+export const GoogleAds = () => (
+  <div>
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>
+
+    <ins
+      class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-7966864664516090"
+      data-ad-slot="3434228844"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+    <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+  </div>
+)
+
+export default GoogleAds
