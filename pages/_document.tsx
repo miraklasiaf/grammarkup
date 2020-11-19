bf2b62251f35f4390887a5a91db57cb22e6376e7
Faisal Karim <faisalkarim96@gmail.com>
2020-11-19 12:51:40 +0700

    refactor: move back all seo tags to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,40 @@ class Document extends NextDocument {
     return (
       <Html lang="en">
         <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
-        <Head />
+        <Head>
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta content="#0694a2" name="theme-color" />
+          <meta content="#0694a2" name="msapplication-TileColor" />
+          <meta content="/static/favicon/browserconfig.xml" name="msapplication-config" />
+          <link href="/static/favicon/favicon.ico" rel="shortcut icon" />
+          <link href="/static/favicon/site.webmanifest" rel="manifest" />
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
+          <link
+            href="/static/favicon/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/static/favicon/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/static/favicon/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link color="#4a9885" href="/static/favicon/safari-pinned-tab.svg" rel="mask-icon" />
+          <meta name="google-site-verification" content={process.env.GOOGLE_SITE_VERIFICATION} />
+          <meta name="monetization" content={process.env.ILP_URL} />
+          {/* <script
+          data-ad-client="ca-pub-7966864664516090"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        /> */}
+        </Head>
         <body>
           <GAScript />
           <ColorModeScript />
