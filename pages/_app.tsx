24147c8f4009a254b3c9daa96e7cde48a4b93404
Faisal Karim <faisalkarim96@gmail.com>
2020-11-19 11:51:04 +0700

    chore: add web monetization api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,29 @@ const App = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta content="#0694a2" name="theme-color" />
+        <meta content="#0694a2" name="msapplication-TileColor" />
+        <meta content="/static/favicon/browserconfig.xml" name="msapplication-config" />
+
+        <link href="/static/favicon/favicon.ico" rel="shortcut icon" />
+        <link href="/static/favicon/site.webmanifest" rel="manifest" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
+        <link href="/static/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/static/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/static/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link color="#4a9885" href="/static/favicon/safari-pinned-tab.svg" rel="mask-icon" />
+        <meta name="google-site-verification" content={process.env.GOOGLE_SITE_VERIFICATION} />
+        <meta name="monetization" content={process.env.ILP_URL} />
+        <script
+          data-ad-client="ca-pub-7966864664516090"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
       </Head>
+
       <DefaultSeo {...siteConfig.seo} />
+
       <Nprogress />
       <ChakraProvider theme={theme} portalZIndex={40}>
         <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,7 @@
-import NextDocument, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext
-} from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import GoogleFonts from 'next-google-fonts'
 import { ColorModeScript } from '@chakra-ui/react'
-import { GAScript } from '@/lib/gtag'
+import { GAScript } from '@/lib/ga'
 
 class Document extends NextDocument {
   static async getInitialProps(ctx: DocumentContext) {
@@ -19,46 +13,7 @@ class Document extends NextDocument {
     return (
       <Html lang="en">
         <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
-        <Head>
-          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          <meta content="#0694a2" name="theme-color" />
-          <meta content="#0694a2" name="msapplication-TileColor" />
-          <meta content="/static/favicon/browserconfig.xml" name="msapplication-config" />
-          <meta
-            name="google-site-verification"
-            content="kFp742NDaDwd1vge_9DiXxitdN8NZX3OA8C4lpzeQXM"
-          />
-          <link href="/static/favicon/favicon.ico" rel="shortcut icon" />
-          <link href="/static/favicon/site.webmanifest" rel="manifest" />
-          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
-          <link
-            href="/static/favicon/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="/static/favicon/favicon-32x32.png"
-            rel="icon"
-            sizes="32x32"
-            type="image/png"
-          />
-          <link
-            href="/static/favicon/favicon-16x16.png"
-            rel="icon"
-            sizes="16x16"
-            type="image/png"
-          />
-          <link
-            color="#4a9885"
-            href="/static/favicon/safari-pinned-tab.svg"
-            rel="mask-icon"
-          />
-          <script
-            data-ad-client="ca-pub-7966864664516090"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          />
-        </Head>
+        <Head />
         <body>
           <GAScript />
           <ColorModeScript />
