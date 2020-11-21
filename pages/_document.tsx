6a3c1f57b30a60f80ca49c8e0ff4ecd6e77319d6
Faisal Karim <faisalkarim96@gmail.com>
2020-11-21 13:32:30 +0700

    chore: enable adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,11 +41,11 @@ class Document extends NextDocument {
           <link color="#4a9885" href="/static/favicon/safari-pinned-tab.svg" rel="mask-icon" />
           <meta name="google-site-verification" content={process.env.GOOGLE_SITE_VERIFICATION} />
           <meta name="monetization" content={process.env.ILP_URL} />
-          {/* <script
-          data-ad-client="ca-pub-7966864664516090"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        /> */}
+          <script
+            data-ad-client="ca-pub-7966864664516090"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          />
         </Head>
         <body>
           <GAScript />
