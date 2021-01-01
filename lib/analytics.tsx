3c203ac560e882a49f5b19e9225f578eccd5638a
Faisal Karim <faisalkarim96@gmail.com>
2021-01-01 21:29:31 +0700

    fix: remove page path

--- lib/analytics.tsx
+++ lib/analytics.tsx
@@ -63,9 +63,7 @@ export const GAScript = () => (
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+            gtag('config', '${GA_TRACKING_ID}');
           `
       }}
     />
